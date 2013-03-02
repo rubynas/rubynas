63d04114fea8e08bca07e0cbfeff8f1b2d116901
Vincent Landgraf <vilandgr@googlemail.com>
2013-03-02 19:48:47 +0100

    added devise with ldap capabilities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate_member!
+  rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
+    render :text => exception, :status => 500
+  end
   protect_from_forgery
 end
