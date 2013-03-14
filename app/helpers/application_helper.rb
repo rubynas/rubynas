bbfb9b4d84fb01f1f82a227a36375a84c18c7ff1
Jens Bissinger <mail@jens-bissinger.de>
2013-03-14 21:57:35 +0100

    add logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def favicon
+    raw %q{<link rel="icon" href="/favicon.ico" type="image/x-icon" />} +
+        %q{<link rel="icon" href="/favicon.png" type="image/png" />}
+  end
 end
