b58fea2e29e061ce424a8f7a23473c0f803c68a2
Jens Bissinger <mail@jens-bissinger.de>
2013-04-10 23:24:01 +0200

    add basic template for afp.conf

--- app/configurations/afp_share_configuration.rb
+++ app/configurations/afp_share_configuration.rb
@@ -1,7 +1,26 @@
 class AfpShareConfiguration < Configuration
   config_file 'afp.conf'
 
-  def time_machine(share)
-    share.options[self][:time_machine] ? 'yes' : 'no'
+  def render
+    @shares = load_shares
+    template.result binding
+  end
+
+private
+
+  def load_shares
+    SharedFolderService.where(service_class: 'AfpShareService')
+  end
+
+  def template
+    ERB.new File.read(__FILE__).split("__END__\n").last
   end
 end
+
+__END__
+
+<% @shares.each do |share| %>
+[<%= share.shared_folder.name %>]
+  path = <%= share.shared_folder.path %>
+  time machine = <%= share.options[:time_machine] == true ? 'yes' : 'no' %>
+<% end %>
--- spec/configurations/afp_share_configuration_spec.rb
+++ spec/configurations/afp_share_configuration_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe AfpShareConfiguration do
-  
+  let('config') { described_class.new }
+  subject { config }
+
+  it 'queries for AfpShareService' do
+    SharedFolderService.should_receive('where')\
+      .with(service_class: 'AfpShareService')\
+      .and_return([])
+    subject.render
+  end
+
+  context 'no shares' do
+    before { config.stub('load_shares').and_return([]) }
+    its('render') { should match(/\A\s*\z/) }
+  end
+
+  context 'shares' do
+    let('share_a') do
+      build(:shared_folder_service)
+    end
+    let('share_b') do
+      build(:shared_folder_service).tap do |s|
+        s.shared_folder.name = 'Bar'
+        s.shared_folder.path = '/bar'
+        s.options[:time_machine] = false
+      end
+    end
+    before { config.stub('load_shares') { [ share_a, share_b ] } }
+
+    its('render') { should include("[System]\n  path = /\n  time machine = yes\n\n"\
+                                   "[Bar]\n  path = /bar\n  time machine = no" ) }
+  end
 end
