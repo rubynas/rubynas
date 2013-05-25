2019e3f5858fbe1e73c1a271d384402918518e85
Jens Bissinger <mail@jens-bissinger.de>
2013-05-26 00:39:56 +0200

    Integrate rubynas-admin project as submodule

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,8 @@
 class MainController < ApplicationController
+  layout false
   before_filter :authenticate_user!
   
   def index
+    render file: Rails.root.join('public', 'index.html')
   end
 end
