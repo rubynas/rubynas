0e9872ebb0c0671f1781a334dbbc9ebc924ccf1b
Jens Bissinger <mail@jens-bissinger.de>
2013-05-26 12:46:36 +0200

    Fix admin app initialization instructions

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,7 +7,14 @@ class MainController < ApplicationController
     if File.exists? admin_app
       render file: admin_app
     else
-      render text: "Cannot find #{admin_app}, please run 'git submodule update --init'"
+      render text: "Cannot find #{admin_app}, "\
+                   "please initialize the admin app:<br><br>"\
+                   "<code>git submodule update --init</code><br><br>"\
+                   "and then build the admin app like so:<br><br>"\
+                   "<code>cd admin</code><br>"\
+                   "<code>npm install</code><br>"\
+                   "<code>bower install</code><br>"\
+                   "<code>brunch build</code>"
     end
   end
 end
